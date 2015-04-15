e9e97c1a37343b3fde666218b4c30e081fa5569c
Devin Torres <devin@devintorr.es>
2015-04-14 19:13:47 -0500

    Cleanup tests

--- test/mandrill-transport.js
+++ test/mandrill-transport.js
@@ -14,10 +14,21 @@ describe('MandrillTransport', function() {
     expect(transport.version).to.equal(packageData.version);
   });
 
-  it('#send', function(done) {
+  describe('#send', function(done) {
     var transport = mandrillTransport();
     var client = transport.mandrillClient;
 
+    var payload = {
+      data: {
+        to: 'SpongeBob SquarePants <spongebob@bikini.bottom>, Patrick Star <patrick@bikini.bottom>',
+        from: 'Gary the Snail <gary@bikini.bottom>',
+        subject: 'Meow...',
+        text: 'Meow!',
+        html: '<p>Meow!</p>'
+      }
+    };
+
+    var status;
     var stub = sinon.stub(client.messages, 'send', function(data, resolve) {
       var message = data.message;
       expect(message).to.exist;
@@ -31,58 +42,57 @@ describe('MandrillTransport', function() {
       expect(message.subject).to.equal('Meow...');
       expect(message.text).to.equal('Meow!');
       expect(message.html).to.equal('<p>Meow!</p>');
-      resolve([{ _id: 'fake-id', status: 'sent' }]);
+      resolve([{ _id: 'fake-id', status: status }]);
     });
 
-    transport.send({
-      data: {
-        to: 'SpongeBob SquarePants <spongebob@bikini.bottom>, Patrick Star <patrick@bikini.bottom>',
-        from: 'Gary the Snail <gary@bikini.bottom>',
-        subject: 'Meow...',
-        text: 'Meow!',
-        html: '<p>Meow!</p>'
-      }
-    }, function(err, info) {
-      expect(stub.calledOnce).to.be.true;
-      expect(err).to.not.exist;
-      expect(info.messageId).to.equal('fake-id');
-      done();
-    });
-  });
-
-  it('#send (queued)', function(done) {
-    var transport = mandrillTransport();
-    var client = transport.mandrillClient;
-
-    var stub = sinon.stub(client.messages, 'send', function(data, resolve) {
-      var message = data.message;
-      expect(message).to.exist;
-      expect(message.to.length).to.equal(2);
-      expect(message.to[0].name).to.equal('SpongeBob SquarePants');
-      expect(message.to[0].email).to.equal('spongebob@bikini.bottom');
-      expect(message.to[1].name).to.equal('Patrick Star');
-      expect(message.to[1].email).to.equal('patrick@bikini.bottom');
-      expect(message.from_name).to.equal('Gary the Snail');
-      expect(message.from_email).to.equal('gary@bikini.bottom');
-      expect(message.subject).to.equal('Meow...');
-      expect(message.text).to.equal('Meow!');
-      expect(message.html).to.equal('<p>Meow!</p>');
-      resolve([{ _id: 'fake-id', status: 'queued' }]);
+    after(function() {
+      stub.restore();
     });
 
-    transport.send({
-      data: {
-        to: 'SpongeBob SquarePants <spongebob@bikini.bottom>, Patrick Star <patrick@bikini.bottom>',
-        from: 'Gary the Snail <gary@bikini.bottom>',
-        subject: 'Meow...',
-        text: 'Meow!',
-        html: '<p>Meow!</p>'
-      }
-    }, function(err, info) {
-      expect(stub.calledOnce).to.be.true;
-      expect(err).to.not.exist;
-      expect(info.messageId).to.equal('fake-id');
-      done();
+    afterEach(function() {
+      stub.reset();
+    });
+
+    function successCallbackFactory(done) {
+      return function successCallback(err, info) {
+        expect(stub.calledOnce).to.be.true;
+        expect(err).to.not.exist;
+        expect(info.messageId).to.equal('fake-id');
+        done();
+      };
+    }
+
+    function errorCallbackFactory(done) {
+      return function errorCallback(err) {
+        expect(stub.calledOnce).to.be.true;
+        expect(err).to.exist;
+        done();
+      };
+    }
+
+    it('sent response', function(done) {
+      status = 'sent';
+      transport.send(payload, successCallbackFactory(done));
+    });
+
+    it('queued response', function(done) {
+      status = 'queued';
+      transport.send(payload, successCallbackFactory(done));
+    });
+
+    it('scheduled response', function(done) {
+      status = 'scheduled';
+      transport.send(payload, errorCallbackFactory(done));
+    });
+
+    it('invalid response', function(done) {
+      status = 'invalid';
+      transport.send(payload, errorCallbackFactory(done));
+    });
+
+    it('rejected response', function(done) {
+      status = 'rejected';
+      transport.send(payload, errorCallbackFactory(done));
     });
   });
 });
