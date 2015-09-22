e23536101e7ffda1dc20239c464af4071ad9ff30
Javier Fernandez <javier.fdez@gmail.com>
2015-09-22 15:51:47 +0200

    Added sendTemplate functionality to match Mandrill's sendTemplate api functionality

--- test/mandrill-transport.js
+++ test/mandrill-transport.js
@@ -220,4 +220,137 @@ describe('MandrillTransport', function() {
       });
     });
   });
+
+  describe('#sendTemplate', function(done) {
+    var transport = mandrillTransport();
+    var client = transport.mandrillClient;
+
+    var payload = {
+      template_name: 'a_template_name',
+      template_content: [{
+          "name": "Michael Knight",
+          "content": "The knight Rider content"
+      }],
+      data: {
+        to: 'SpongeBob SquarePants <spongebob@bikini.bottom>, Patrick Star <patrick@bikini.bottom>',
+        cc: 'Somefool Gettingcopied <somefool@example.com>, Also Copied <alsocopied@example.com>',
+        bcc: 'silentcopy@example.com, alsosilent@example.com',
+        from: 'Gary the Snail <gary@bikini.bottom>',
+        subject: 'Meow...',
+        text: 'Meow!',
+        html: '<p>Meow!</p>'
+      }
+    };
+
+    var status;
+    var stub = sinon.stub(client.messages, 'sendTemplate', function(data, resolve) {
+      expect(data.async).to.equal(false);
+
+      var template_name = data.template_name;
+      expect(template_name).to.exist;
+      expect(template_name).to.equal('a_template_name');
+
+      var template_content = data.template_content;
+      expect(template_content).to.exist;
+      expect(template_content.length).to.equal(1);
+      expect(template_content[0].name).to.equal('Michael Knight');
+      expect(template_content[0].content).to.equal('The knight Rider content');
+
+      var message = data.message;
+      expect(message).to.exist;
+      expect(message.to.length).to.equal(6);
+      expect(message.to[0].name).to.equal('SpongeBob SquarePants');
+      expect(message.to[0].email).to.equal('spongebob@bikini.bottom');
+      expect(message.to[1].name).to.equal('Patrick Star');
+      expect(message.to[1].email).to.equal('patrick@bikini.bottom');
+      expect(message.to[2].name).to.equal('Somefool Gettingcopied');
+      expect(message.to[2].email).to.equal('somefool@example.com');
+      expect(message.to[2].type).to.equal('cc');
+      expect(message.to[3].name).to.equal('Also Copied');
+      expect(message.to[3].email).to.equal('alsocopied@example.com');
+      expect(message.to[3].type).to.equal('cc');
+      expect(message.to[4].email).to.equal('silentcopy@example.com');
+      expect(message.to[4].type).to.equal('bcc');
+      expect(message.to[5].email).to.equal('alsosilent@example.com');
+      expect(message.to[5].type).to.equal('bcc');
+      expect(message.from_name).to.equal('Gary the Snail');
+      expect(message.from_email).to.equal('gary@bikini.bottom');
+      expect(message.subject).to.equal('Meow...');
+      expect(message.text).to.equal('Meow!');
+      expect(message.html).to.equal('<p>Meow!</p>');
+
+      expect(message.tags).to.be.an.array;
+      expect(message.metadata).to.be.an.array;
+      expect(message.recipient_metadata).to.be.an.array;
+      expect(message.preserve_recipients).to.equal(true);
+
+      resolve([{ _id: 'fake-id', status: status }]);
+    });
+
+    after(function() {
+      stub.restore();
+    });
+
+    afterEach(function() {
+      stub.reset();
+    });
+
+    it('sent response', function(done) {
+      status = 'sent';
+      transport.sendTemplate(payload, function(err, info) {
+        expect(err).to.not.exist;
+        expect(stub.calledOnce).to.be.true;
+        expect(info.accepted.length).to.equal(1);
+        expect(info.rejected.length).to.equal(0);
+        expect(info.messageId).to.equal('fake-id');
+        done();
+      });
+    });
+
+    it('queued response', function(done) {
+      status = 'queued';
+      transport.sendTemplate(payload, function(err, info) {
+        expect(err).to.not.exist;
+        expect(stub.calledOnce).to.be.true;
+        expect(info.accepted.length).to.equal(1);
+        expect(info.rejected.length).to.equal(0);
+        expect(info.messageId).to.equal('fake-id');
+        done();
+      });
+    });
+
+    it('scheduled response', function(done) {
+      status = 'scheduled';
+      transport.sendTemplate(payload, function(err, info) {
+        expect(err).to.not.exist;
+        expect(stub.calledOnce).to.be.true;
+        expect(info.accepted.length).to.equal(1);
+        expect(info.rejected.length).to.equal(0);
+        expect(info.messageId).to.equal('fake-id');
+        done();
+      });
+    });
+
+    it('invalid response', function(done) {
+      status = 'invalid';
+      transport.sendTemplate(payload, function(err, info) {
+        expect(err).to.not.exist;
+        expect(stub.calledOnce).to.be.true;
+        expect(info.accepted.length).to.equal(0);
+        expect(info.rejected.length).to.equal(1);
+        done();
+      });
+    });
+
+    it('rejected response', function(done) {
+      status = 'rejected';
+      transport.sendTemplate(payload, function(err, info) {
+        expect(err).to.not.exist;
+        expect(stub.calledOnce).to.be.true;
+        expect(info.accepted.length).to.equal(0);
+        expect(info.rejected.length).to.equal(1);
+        done();
+      });
+    });
+  });
 });
